38f3f6030e1981a8d9531caeff5f4e675c1bf680
michaellyon <michaellyon@localhost.localdomain>
2020-11-23 14:59:08 -0600

    Created a folder for text files

---
+++ files_demo.py
@@ -0,0 +1,8 @@
+fhandle = open("mbox.txt")
+# line = fhandle.read()
+# print(line[0:10]) # slicing.
+
+
+file_1 = open("demo.txt", "w")
+# file_1 = open("demo.txt", "a")
+file_1.write("How are you")
